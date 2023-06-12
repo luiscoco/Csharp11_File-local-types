247060fa04b8b21c0fe4fa1e828680b3b6dc9aff
Luis Coco Enríquez <luiscocoenriquez@hotmail.com>
2023-06-12 19:05:12 +0200

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿MyClass myClass = new MyClass();
+MyClass.MyMethod();
+
+
+//Beginning in C# 11, you can use the "file" access modifier
+//to create a type whose visibility is scoped to the source
+//file in which it is declared. This feature helps source generator
+//authors avoid naming collisions.
+file class MyClass
+{
+    public static void MyMethod()
+    {
+        Console.WriteLine("Hello from MyMethod!");
+    }
+}
